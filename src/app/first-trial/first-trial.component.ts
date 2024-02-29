0c045e300e1eae3b7f65d7626e232afde3bd082b
Mutagah <kihahumutagah@gmail.com>
2024-02-29 10:49:52 +0300

    :lipstick:feat:Generation of first-trial component.

---
+++ src/app/first-trial/first-trial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-trial',
+  templateUrl: './first-trial.component.html',
+  styleUrls: ['./first-trial.component.scss']
+})
+export class FirstTrialComponent {
+
+}
