53a46b00f67cbaaf6a81367a26f400cc8ad97cea
Mutagah <kihahumutagah@gmail.com>
2024-02-29 10:49:11 +0300

    :lipstick:feat:data binding quick demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_revision';
+  title = 'Angular Revision';
+
+  //property binding > Used when you want to assign dynamic values. 
+  surName :string = "Kihahu"
+  firstName :string = "Mutaga"
+
+
+  interpolationOfData(){
+    return "data binding practice via interpolation"
+  }
+
+  // Event binding
+  changeInputValue(){
+    this.surName = "Mwangi"
+    this.firstName = "Ndungu"
+  }
+
+  returnInputValue(){
+    this.surName = "Kihahu"
+    this.firstName = "Mutaga"
+  }
+
+  // Two way Data Binding
+  twoWayBinding(event : any){
+    this.surName= event.target.value
+  }
+  
+
 }
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FirstTrialComponent } from './first-trial/first-trial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTrialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
