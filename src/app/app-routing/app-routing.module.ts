9a3159b7d25befd98960df15a4ac498ec696ea9b
Mutagah <kihahumutagah@gmail.com>
2024-03-05 17:24:04 +0300

    :sparkles:feat:Addition of the pipes route.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,6 +6,7 @@ import { Day2Component } from '../day2/day2.component';
 import { HomeComponent } from '../home/home.component';
 import { FirstTrialComponent } from '../first-trial/first-trial.component';
 import { Day3Component } from '../day3/day3.component';
+import { PipesComponent } from '../pipes/pipes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,8 +17,12 @@ const routes: Routes = [
   },
   {
     path: 'day3',
-    component: Day3Component
-  }
+    component: Day3Component,
+  },
+  {
+    path: 'pipes',
+    component: PipesComponent,
+  },
 ];
 @NgModule({
   declarations: [],
